3b928bf2b0d721b07dab149ebb3480ff1c471d13
Adam Mubarak Bako <mubarakadambako@gmail.com>
2026-07-23 15:28:05 +0000

    fix path

--- frontend/src/hooks/useAuth.js
+++
@@ -1,6 +0,0 @@
-import { useAuthStore } from '../store/authStore';
-
-export const useAuth = () => {
-  const { user, token, setAuth, logout } = useAuthStore();
-    return { user, token, setAuth, logout, isLoggedIn: !!token };
-    };
--- frontend/src/services/requisition.service.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-export const getAll   = (params) => api.get('/requisitions', { params });
-export const getOne   = (id)     => api.get(`/requisitions/${id}`);
-export const create   = (data)   => api.post('/requisitions', data);
-export const update   = (id, d)  => api.put(`/requisitions/${id}`, d);
-export const remove   = (id)     => api.delete(`/requisitions/${id}`);
-export const submit   = (id)     => api.post(`/requisitions/${id}/submit`);
-export const withdraw = (id)     => api.post(`/requisitions/${id}/withdraw`);
