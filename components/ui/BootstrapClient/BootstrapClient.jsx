cae66e5ecfcaca98543d3e550a41035caafd8980
Adam Mubarak Bako <mubarakadambako@gmail.com>
2026-07-26 05:30:31 +0100

    Refactor BootstrapClient to use dynamic import

--- components/ui/BootstrapClient/BootstrapClient.jsx
+++ components/ui/BootstrapClient/BootstrapClient.jsx
@@ -1,10 +1,9 @@
 'use client';
-
 import { useEffect } from 'react';
 
 export default function BootstrapClient() {
   useEffect(() => {
-      require('bootstrap/dist/js/bootstrap.bundle.min.js');
-        }, []);
-          return null;
-          }
+    import('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }, []);
+  return null;
+}
