77962fd6fc8185c9b5890ae30aeeb4f3aae3e018
Adam Mubarak Bako <mubarakadambako@gmail.com>
2026-07-27 04:48:12 +0100

    Add hydration state to auth store

--- store/authStore.js
+++ store/authStore.js
@@ -1,24 +1,30 @@
 'use client';
-
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { create }   from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 export const useAuthStore = create(
   persist(
-      (set) => ({
-            token: null,
-                  user:  null,
+    (set) => ({
+      token:    null,
+      user:     null,
+      hydrated: false,
 
-                        setAuth: (token, user) => set({ token, user }),
+      setAuth: (token, user) => set({ token, user }),
 
-                              updateUser: (updates) =>
-                                      set((s) => ({ user: { ...s.user, ...updates } })),
+      updateUser: (updates) =>
+        set((s) => ({ user: { ...s.user, ...updates } })),
 
-                                            logout: () => set({ token: null, user: null }),
-                                                }),
-                                                    {
-                                                          name:    'ksu-auth',
-                                                                version: 1,
-                                                                    }
-                                                                      )
-                                                                      );
+      logout: () => set({ token: null, user: null }),
+
+      setHydrated: () => set({ hydrated: true }),
+    }),
+    {
+      name:    'ksu-auth',
+      version: 1,
+      storage: createJSONStorage(() => localStorage),
+      onRehydrateStorage: () => (state) => {
+        state?.setHydrated();
+      },
+    }
+  )
+);
