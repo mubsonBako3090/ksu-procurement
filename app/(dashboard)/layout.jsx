ad116c952ca365733493e505bc27b28e70d43bff
Adam Mubarak Bako <mubarakadambako@gmail.com>
2026-07-27 05:22:25 +0100

    Format DashboardLayout component for consistency

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,10 +1,10 @@
-import AuthGuard from '@/components/layout/AuthGuard/AuthGuard';
+import AuthGuard   from '@/components/layout/AuthGuard/AuthGuard';
 import PageWrapper from '@/components/layout/PageWrapper/PageWrapper';
 
 export default function DashboardLayout({ children }) {
   return (
-      <AuthGuard>
-            <PageWrapper>{children}</PageWrapper>
-                </AuthGuard>
-                  );
-                  }
+    <AuthGuard>
+      <PageWrapper>{children}</PageWrapper>
+    </AuthGuard>
+  );
+}
